2df70e0a75b9855693ec17b3aa8aa62bd6ca8d13
gabriel250206 <158100593+gabriel250206@users.noreply.github.com>
2025-01-14 21:15:02 -0600

    lesto

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +8,18 @@ import { Component } from '@angular/core';
   standalone: false,
 })
 export class HomePage {
-
+  nombre = new FormControl('');
+  cambiarNombre(){
+    this.usuario.controls.nombre.setValue('Ornitorrinco');
+  }
+  usuario = new FormGroup({
+    nombre: new FormControl('', [Validators.required, Validators.minLength(4)]),
+    email: new FormControl('', Validators.email),
+    edad: new FormControl(),
+  });
+  guardarDatos(){
+    console.log(this.usuario.value);
+  }
   constructor() {}
 
 }
